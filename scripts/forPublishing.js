fc25ca6fd914b5df58adeeb9ee0b93c61f423c07
Bruce McPherson <bruce@mcpher.com>
2015-06-24 13:30:22 +0100

    updated by GasGit automation

--- scripts/forPublishing.js
+++ scripts/forPublishing.js
@@ -7,7 +7,7 @@ function getLibraryInfo () {
   return { 
     info: {
       name:'ColorArranger',
-      version:'0.0.7',
+      version:'0.0.8',
       key:'MkxQ8A98nZ7NLSeDbsNDEZKi_d-phDA33',
       description:'addon for sorting sheet by colors',
       share:'https://script.google.com/d/1eSvQMHBpjkt13USxpH4CFOp9mxzyr5hv08M4E3Iz1Cw7gDxxoStH4eLU/edit?usp=sharing'
